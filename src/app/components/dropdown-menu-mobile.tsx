7877fe8e72fe43dbb6a9010f3b7b4f2f8bd64355
iPaulinh0 <pauloaraujo2607@gmail.com>
2025-03-05 19:36:24 -0300

    fix: mobile hamburguer menu

--- src/app/components/dropdown-menu-mobile.tsx
+++ src/app/components/dropdown-menu-mobile.tsx
@@ -10,7 +10,7 @@ export function DropdownMenuMobile() {
                     <Menu size={24}/>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent side="bottom" align="end" className="mt-3 bg-zinc-950 border-zinc-800">
-                    <DropdownMenuItem>
+                    <DropdownMenuItem className="text-zinc-200">
                         <Image src="https://github.com/iPaulinh0.png" alt="profile picture" className="h-6 w-6 rounded-full" width={24} height={24}/>
                         Profile
                     </DropdownMenuItem>
